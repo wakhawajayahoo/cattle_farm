a85a3e4a87b80ccf1cebc4b8b5efaa976e3c28e2
Anwender <Anwender@Anwender-PC>
2020-10-02 19:46:36 +0500

    first comit cattle_farm

---
+++ cattlefarm/src/cattlefarm/animal.java
@@ -0,0 +1,26 @@
+package cattlefarm;
+
+public class animal {
+	private int id;
+	private char sex;
+	private int dob;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public int getDob() {
+		return dob;
+	}
+	public void setDob(int dob) {
+		this.dob = dob;
+	}
+
+}
